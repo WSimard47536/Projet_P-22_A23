afb3e39e13d799b30fe35d9fe163a3b7fed8ec93
LyamBRS <cous5830@gmail.com>
2023-10-04 13:03:22 -0400

    Distance now tests multiple distance to make

--- src/main.cpp
+++ src/main.cpp
@@ -18,20 +18,20 @@ void loop()
     #ifdef ISTEST
      // test_ROBUSMovement_moveStraight();
     #endif
-    ROBUSMovement_moveStraight(FORWARD, 20, 50.00f);
+    ROBUSMovement_moveStraight(FORWARD, 75, 10.00f);
     delay(2000);
-    ROBUSMovement_moveStraight(FORWARD, 20, 50.00f);
+    ROBUSMovement_moveStraight(FORWARD, 75, 20.00f);
     delay(2000);
-    ROBUSMovement_moveStraight(FORWARD, 20, 50.00f);
+    ROBUSMovement_moveStraight(FORWARD, 75, 30.00f);
     delay(2000);
-    ROBUSMovement_moveStraight(FORWARD, 20, 50.00f);
+    ROBUSMovement_moveStraight(FORWARD, 75, 40.00f);
     delay(2000);
-    ROBUSMovement_moveStraight(FORWARD, 20, 50.00f);
+    ROBUSMovement_moveStraight(FORWARD, 75, 50.00f);
     delay(2000);
-    ROBUSMovement_moveStraight(FORWARD, 20, 50.00f);
+    ROBUSMovement_moveStraight(FORWARD, 75, 60.00f);
     delay(2000);
-    ROBUSMovement_moveStraight(FORWARD, 20, 50.00f);
+    ROBUSMovement_moveStraight(FORWARD, 75, 70.00f);
     delay(2000);
-    ROBUSMovement_moveStraight(FORWARD, 20, 50.00f);
+    ROBUSMovement_moveStraight(FORWARD, 75, 80.00f);
   }
 }
