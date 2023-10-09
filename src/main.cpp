bb7a672c44c4a9868f18e4c1a883928131a801a1
Christophe Boutin <christo.boutin00@gmail.com>
2023-10-09 14:00:57 -0400

    changes to the 50cm mark

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,30 @@ void setup()
 
 void loop()
 {
-  //if (ROBUS_IsBumper(REAR_BUMPER))
-  //{
-  //  #ifdef ISTEST
-  //    test_ROBUSMovement_moveStraight();
-  //  #endif
-  //}
+  
+  ROBUSMovement_moveStraight(FORWARD, 100, 50.0f);
+  for (;;)
+  {
 
-  
-  ROBUSMovement_turnOnSelf(-1, 0.10, 90);
-  //ROBUSMovement_turnOnSelf(1, 0.10, 1080);
+  }
+  /*delay(2000);
+  ROBUSMovement_moveStraight(FORWARD, 100, 20.0f);
   delay(2000);
-  
+  ROBUSMovement_moveStraight(FORWARD, 100, 30.0f);
+  delay(2000);
+  ROBUSMovement_moveStraight(FORWARD, 100, 40.0f);
+  delay(2000);
+  ROBUSMovement_moveStraight(FORWARD, 100, 50.0f);
+  delay(2000);
+  ROBUSMovement_moveStraight(FORWARD, 100, 60.0f);
+  delay(2000);
+  ROBUSMovement_moveStraight(FORWARD, 100, 70.0f);
+  delay(2000);
+  ROBUSMovement_moveStraight(FORWARD, 100, 80.0f);
+  delay(2000);
+  ROBUSMovement_moveStraight(FORWARD, 100, 90.0f);
+  delay(2000);
+  ROBUSMovement_moveStraight(FORWARD, 100, 100.0f);
+  delay(2000);
+  */
 }
